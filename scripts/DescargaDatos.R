50acc74d487ada483bbb7f7791fe8d8d7e432d24
Alu0100673647 <alu0100673647@ull.edu.es>
2017-10-14 19:28:26 +0100

    limpiando datos recogidos del Twitter, aunque falta que funcione  el eliminar plabras vacias

--- scripts/DescargaDatos.R
+++ scripts/DescargaDatos.R
@@ -1,58 +1,60 @@
 # Cargar libreria de TwitterR
 
 library("twitteR");
+library("tm");
+library("RTextTools");
+library("SnowballC");
+library("e1071");
+library("randomForest");
+library("stringi");
+library("reader");
+#library("worldcloud");
+#libs<-c('tm','RTextTools','','tm','e1071','randomForest','stringi')
+#lapply(libs,require, character.only= TRUE)
 
 # Leer fichero credenciales
-
+# cuando ejecutamos el PIN lo teniamos qu poner en consola para   q crear el ficher myoauth
 source('credenciales.R')
-
+source('spanish.txt', encoding = "UTF-8")
 # Función para buscar #hastags, @users, words
 
-tweets <- searchTwitter("Tenerife", n=10, lang="es")
+tweets <- searchTwitter("Canarias", n=10, lang="es",since="2017-06-01")
+
+# vuelca la informacion de los tweets a un data frame
+df = twListToDF(tweets)
+
+# obtiene el texto de los tweets
+txt = df$text
+
+##### inicio limpieza de datos #####
+# remueve retweets
+txtclean = gsub("(RT|via)((?:\\b\\W*@\\w+)+)", "", txt)
+# remove @otragente
+txtclean = gsub("@\\w+", "", txtclean)
+# remueve simbolos de puntuación
+txtclean = gsub("[[:punct:]]", "", txtclean)
+# remove números
+txtclean = gsub("[[:digit:]]", "", txtclean)
+# remueve links
+txtclean = gsub("http\\w+", "", txtclean)
+##### fin limpieza de datos #####
 
 
+# construye un corpus
+corpus = Corpus(VectorSource(txtclean))
+
+# convierte a minúsculas
+corpus = tm_map(corpus, tolower)
+# remueve palabras vacías (stopwords) en español
+corpus <- tm_map(corpus,removeWords,stopwords("spanish"))
+
+# carga archivo de palabras vacías personalizada y lo convierte a ASCII
+sw <- readLines("spanish.txt",encoding="UTF-8")
+sw = iconv(sw, to="ASCII//TRANSLIT")
+# remueve palabras vacías personalizada
+corpus = tm_map(corpus, removeWords, sw)
+# remove espacios en blanco extras
+corpus = tm_map(corpus, stripWhitespace)
 
 
-# Coger solo el primer tweet para datos concretos del mismo
-
-#tweet <- tweets[[1]];
-
-# Mostrar estructura del tweet
-
-#str(tweet)
-
-# Funcion para buscar retweets
-
-#retweets <- Rtweets(n=25, lang="es", since=NULL)
-
-#retweet <- retweets[[1]];
-
-#str(retweet)
-# Obtener información del usuario
-
-#user <- getUser(tweet$getScreenName());
-
-
-# Estructura del usuario
-
-#str(user)
-
-# Nombre del usuario
-
-#user$getName()
-#user$getId()
-# Obtener el texto del tweet:
-
-#tweet$getText()
-
-# Obtener el texto del retweet:
-#retweet$getText()
-
-#for(i in 1:10) {
-  #print(tweets)
- # tweet <- tweets[[i]];
- # print(str(tweet$getText()))
-  #user <- getUser(tweet$getScreenName());
- # print(user)
-  
-#}
+corpus = tm_map(corpus,removeWords)
